76242bec4abf65c39ed50dc797b29b85a7e4ef63
Jheyele Raquel <jheyele.xavier@atos.net>
2022-06-20 13:51:55 -0300

    update

--- src/server.ts
+++ src/server.ts
@@ -3,10 +3,9 @@ import { routes } from "./routes";
 import cors from "cors";
 
 const app = express();
-const port = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log("Server is running"));
+app.listen(process.env.PORT || 8080, () => console.log("Server is running"));
